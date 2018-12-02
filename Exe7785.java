a5f165353046095d23e5d90ad7b074e7bb9944af
Philipp <pja21393@gmail.com>
2018-12-02 14:36:40 -0500

    first commit

---
+++ Exe7785.java
@@ -0,0 +1,15 @@
+// Philipp Adams cs435 7785 mp
+import java.io.*;
+
+class Exe7785
+{
+  public static void main(String args[])
+  {
+    if(args.length == 3)
+    {
+      int iterations = args[0];
+      int initialValue = args[1];
+      int fileName = args[2];
+    }
+  }
+}
---
+++ PageRank7785.java
@@ -0,0 +1,4 @@
+class PageRank7785
+{
+
+}
